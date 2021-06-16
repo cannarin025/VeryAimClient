d6cc94c6d80b141caa1db4cbbac4edeed633e5b0
cannarin025 <cannarin025@gmail.com>
2021-06-16 22:32:22 +0100

    Finished aimbot and fixed snapping to non-existent entities

--- VeryAimClient/Cheats.cpp
+++ VeryAimClient/Cheats.cpp
@@ -15,6 +15,7 @@ DWORD procID;
 HANDLE procH;
 uintptr_t moduleBase;
 
+//Utils
 //function to get base address of module
 uintptr_t GetModuleBaseAddress(const char* modName) {
 	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procID);
@@ -69,12 +70,132 @@ struct glowStructAlly {
 	BYTE fullBloom = false;
 } allyGlow;
 
+struct view_matrix_t {
+	float matrix[16];
+} playerView;
+
+class Vector3 {
+public:
+	float x, y, z;
+	Vector3() : x(0.f), y(0.f), z(0.f) {}
+	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+};
+
+Vector3 entityLocation(uintptr_t entity) { //Stores XYZ coordinates in a Vector3.
+	return RPM<Vector3>(entity + m_vecOrigin);
+}
+
+Vector3 GetHead(uintptr_t player) {
+	struct boneMatrix_t {
+		byte pad3[12];
+		float x;
+		byte pad1[12];
+		float y;
+		byte pad2[12];
+		float z;
+	};
+	uintptr_t boneBase = RPM<uintptr_t>(player + m_dwBoneMatrix);
+	boneMatrix_t boneMatrix = RPM<boneMatrix_t>(boneBase + (sizeof(boneMatrix) * 8 /*8 is the boneid for head*/));
+	return Vector3(boneMatrix.x, boneMatrix.y, boneMatrix.z);
+}
+
+struct Vector3 WorldToScreen(const struct Vector3 pos, struct view_matrix_t matrix) { //This turns 3D coordinates (ex: XYZ) int 2D coordinates (ex: XY).
+	struct Vector3 out;
+	float _x = matrix.matrix[0] * pos.x + matrix.matrix[1] * pos.y + matrix.matrix[2] * pos.z + matrix.matrix[3];
+	float _y = matrix.matrix[4] * pos.x + matrix.matrix[5] * pos.y + matrix.matrix[6] * pos.z + matrix.matrix[7];
+	out.z = matrix.matrix[12] * pos.x + matrix.matrix[13] * pos.y + matrix.matrix[14] * pos.z + matrix.matrix[15];
+
+	_x *= 1.f / out.z;
+	_y *= 1.f / out.z;
+
+	out.x = screen_width * .5f;
+	out.y = screen_height * .5f;
+
+	out.x += 0.5f * _x * screen_width + 0.5f;
+	out.y -= 0.5f * _y * screen_height + 0.5f;
+
+	return out;
+}
+
+int getTeam(uintptr_t entity) {
+	return RPM<int>(entity + m_iTeamNum);
+}
+
+uintptr_t GetPlayer() {
+	return RPM< uintptr_t>(moduleBase + dwLocalPlayer);
+}
+
+uintptr_t GetEntity(int index) {  //Each player has an index. 1-64
+	return RPM< uintptr_t>(moduleBase + dwEntityList + index * 0x10); //We multiply the index by 0x10 to select the player we want in the entity list.
+}
+
+int GetHealth(uintptr_t entity) {
+	return RPM<int>(entity + m_iHealth);
+}
+
+bool DormantCheck(uintptr_t player) {
+	return RPM<int>(player + m_bDormant);
+}
+
+float GetScreenDistance(int x1, int y1, int x2, int y2) {
+	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+int FindClosestEnemy() {
+	float finish;
+	int closestEntityIndex = 1;
+	Vector3 calc = { 0, 0, 0 };
+	float closest = FLT_MAX;
+	int localTeam = getTeam(GetPlayer());
+	for (int i = 1; i < 64; i++) { //Loops through all the entitys in the index 1-64.
+		DWORD entity = GetEntity(i);
+		int enmTeam = getTeam(entity); if (enmTeam == localTeam) continue;
+		int enmHealth = GetHealth(entity); if (enmHealth < 1 || enmHealth > 100) continue;
+		int dormant = DormantCheck(entity); if (dormant) continue;
+		Vector3 headBone = WorldToScreen(GetHead(entity), playerView);
+		finish = GetScreenDistance(headBone.x, headBone.y, xhairx, xhairy);
+		if (finish < closest) {
+			closest = finish;
+			closestEntityIndex = i;
+		}
+	}
+	DWORD closestEntity = GetEntity(closestEntityIndex);
+	int closestHp = GetHealth(closestEntity);
+	int isDormant = DormantCheck(closestEntity);
+	if (isDormant || (closestHp < 1 || closestHp > 100)) {
+		closestEntityIndex = NULL;
+	}
+	return closestEntityIndex;
+}
+
+HDC hdc;
+void DrawLine(float StartX, float StartY, float EndX, float EndY) { //This function is optional for debugging.
+	int a, b = 0;
+	HPEN hOPen;
+	HPEN hNPen = CreatePen(PS_SOLID, 2, 0x0000FF /*red*/);
+	hOPen = (HPEN)SelectObject(hdc, hNPen);
+	MoveToEx(hdc, StartX, StartY, NULL); //start of line
+	a = LineTo(hdc, EndX, EndY); //end of line
+	DeleteObject(SelectObject(hdc, hOPen));
+}
+
+//find enemy thread setup
+int closestEnemyIndex;
+void FindClosestEnemyThread() {
+	while (1) {
+		closestEnemyIndex = FindClosestEnemy();
+	}
+}
+
+
+
 int main() {
 	//setup
 	hwnd = FindWindowA(NULL, "Counter-Strike: Global Offensive");
 	GetWindowThreadProcessId(hwnd, &procID);
 	moduleBase = GetModuleBaseAddress("client.dll");
 	procH = OpenProcess(PROCESS_ALL_ACCESS, NULL, procID);
+	hdc = GetDC(hwnd);
 
 	//hack configuration
 	bool aimbot = true;
@@ -83,19 +204,30 @@ int main() {
 	bool esp = true;
 	bool bHop = true;
 
-	while (!GetAsyncKeyState(VK_END)) {
-		uintptr_t localPlayer = RPM<uintptr_t>(moduleBase + dwLocalPlayer);
-		int localTeam = RPM<int>(localPlayer + m_iTeamNum);
-		uintptr_t glowManager = RPM<uintptr_t>(moduleBase + dwGlowObjectManager);
+	//creates thread to find closest enemy to player
+	if (aimbot) {
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)FindClosestEnemyThread, NULL, NULL, NULL);
+	}
 
-		int playerHP = RPM<int>(localPlayer + m_iHealth);
-		if (playerHP < 100) {
-			WPM<int>(playerHP, 100);
+	while (!GetAsyncKeyState(VK_END)) {
+		uintptr_t glowManager = RPM<uintptr_t>(moduleBase + dwGlowObjectManager);
+		
+		//player vars
+		uintptr_t player = RPM<uintptr_t>(moduleBase + dwLocalPlayer);
+		int playerTeam = RPM<int>(player + m_iTeamNum);
+		playerView = RPM<view_matrix_t>(moduleBase + dwViewMatrix);
+		int playerHP = RPM<int>(player + m_iHealth); //godmode (not working)
+		int playerArmour = RPM<int>(player + m_ArmorValue);
+
+
+		if (playerHP < 10000 or playerArmour < 10000) {
+			WPM<int>(player + m_iHealth, 10000);
+			WPM<int>(player + m_ArmorValue, 10000);
 		}
 
 		if (bHop) {
 			uintptr_t buffer;
-			int flags = RPM<int>(localPlayer + m_fFlags);
+			int flags = RPM<int>(player + m_fFlags);
 			if (flags & 1) {
 				buffer = 5;
 			}
@@ -110,20 +242,20 @@ int main() {
 		if (aimbot || triggerbot || radar || esp) {
 			for (int i = 1; i < 64; i++) {
 				DWORD currentEntity = RPM<DWORD>(moduleBase + dwEntityList + i * 0x10);
-				int hp = RPM<int>(currentEntity + m_iHealth);
+				int entityHp = RPM<int>(currentEntity + m_iHealth);
 				int dormant = RPM<int>(currentEntity + m_bDormant);
 				//check to ensure entity is alive and not dormant
-				if (hp < 1 || hp > 100) continue;
+				if (entityHp < 1 || entityHp > 100) continue;
 				if (dormant) continue;
 
-				int team = RPM<int>(currentEntity + m_iTeamNum);
+				int entityTeam = RPM<int>(currentEntity + m_iTeamNum);
 
 				if (esp) {
 					int glowIndex = RPM<int>(currentEntity + m_iGlowIndex);
-					if (team == localTeam) {
+					if (entityTeam == playerTeam) {
 						WPM<glowStructAlly>(glowManager + (glowIndex * 0x38) + 0x4, allyGlow);
 					}
-					else if (team != localTeam) {
+					else if (entityTeam != playerTeam) {
 						WPM <glowStructEnemy>(glowManager + (glowIndex * 0x38) + 0x4, enemyGlow);
 					}
 				}
@@ -133,6 +265,16 @@ int main() {
 						WPM<bool>(currentEntity + m_bSpotted, true);
 					}
 					//std::cout << "spotted! \n";
+				}
+
+				if (aimbot) {
+					if (closestEnemyIndex != NULL) {
+						Vector3 closestw2shead = WorldToScreen(GetHead(GetEntity(closestEnemyIndex)), playerView);
+						//DrawLine(xhairx, xhairy, closestw2shead.x, closestw2shead.y); //optinal for debugging
+
+						if (GetAsyncKeyState(VK_MENU /*alt key*/) && closestw2shead.z >= 0.001f /*onscreen check*/)
+							SetCursorPos(closestw2shead.x, closestw2shead.y); //turn off "raw input" in CSGO settings
+					}
 				}
 			}
 		}
